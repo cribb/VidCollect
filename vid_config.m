dd568e43ad6fe78b479fb399bf8d8aa09401279f
Megan Loo Pramojaney <pramoj23@cs.unc.edu>
2020-07-20 13:55:21 -0400

    deleted unnecessary buttons on vid_app and added vid_config file

---
+++ vid_config.m
@@ -0,0 +1,18 @@
+function Video = vid_config()
+% VID_CONFIG configures the demo camera
+%
+
+    Video.CameraName = 'Grasshopper3';
+    Video.ExposureMode = 'off';
+    Video.FrameRateMode = 'off';
+    Video.ShutterMode = 'manual';
+    Video.Gain = 10;
+    Video.Gamma = 1.15;
+    Video.Brightness = 5.8594;
+    Video.Format = [];
+    Video.Height = 768;
+    Video.Width = 1024;
+    Video.Depth = 16;
+    Video.ExposureTime = 8; % [ms]
+
+
